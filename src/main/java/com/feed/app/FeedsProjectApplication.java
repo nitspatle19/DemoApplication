fc4b6fd0f685a3719f59aacc8d72f91d79d6dcde
nitspatle19 <nitspatle19@gmail.com>
2024-04-17 00:49:45 -0700

    my first commit

---
+++ src/main/java/com/feed/app/FeedsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.feed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feed/app/FeedsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feed.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
